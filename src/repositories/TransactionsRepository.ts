b69a5478a71a1d890640b3e623138d40c658fb3f
Elves Trindade <elvestrindade31@gmail.com>
2020-05-06 13:14:14 +0100

    :sparkles: challange finalized

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -5,6 +5,11 @@ interface Balance {
   outcome: number;
   total: number;
 }
+interface CreteTransitionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 
 class TransactionsRepository {
   private transactions: Transaction[];
@@ -14,15 +19,42 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const { income, outcome } = this.transactions.reduce(
+      (acomulator: Balance, transaction: Transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            acomulator.income += transaction.value;
+            break;
+          case 'outcome':
+            acomulator.outcome += transaction.value;
+            break;
+          default:
+            break;
+        }
+        return acomulator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+    const total = income - outcome;
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreteTransitionDTO): Transaction {
+    const transition = new Transaction({
+      title,
+      value,
+      type,
+    });
+    this.transactions.push(transition);
+    return transition;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,11 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,9 +13,22 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    if (!['income', 'outcome'].includes(type)) {
+      throw new Error('Transaction type is invalid');
+    }
+
+    const { total } = this.transactionsRepository.getBalance();
+    if (type === 'outcome' && total < value) {
+      throw new Error('You dont have enough balance');
+    }
+    const transation = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transation;
   }
 }
-
 export default CreateTransactionService;
